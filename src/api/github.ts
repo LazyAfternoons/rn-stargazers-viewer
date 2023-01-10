3508df6c13e5a5b6c84a902c096540e32f58ec10
LazyAfternoons <lazyafternoons@outlook.it>
2023-01-11 00:37:21 +0100

    feat: add github stargazers api

---
+++ src/api/github.ts
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+/** Creates an axios instance with github's API base URL */
+const axiosInst = axios.create({
+  baseURL: 'https://api.github.com',
+});
+
+/**
+ * Gets the list of users who have added the given repository to their favorites, namely stargazers
+ *
+ * @remarks
+ * API documentation can be found {@link https://docs.github.com/en/rest/activity/starring?apiVersion=2022-11-28 | here}.
+ *
+ * @param owner - The owner of the repository.
+ * @param repo - The repository name.
+ * @param perPage - The number of users returned per page.
+ * @param page - The number of the requested page, starts from 1.
+ * @param withTimeStamp - True if the starred_at timestamp has to be included, false otherwise. If true returns a Starred array.
+ * @returns An array of users of Starred if withTimestamp is true, an array of User otherwise.
+ */
+export async function getStargazers({
+  owner,
+  repo,
+  perPage,
+  page,
+  withTimeStamp,
+}: getStargazersPayload): Promise<Array<User | Starred>> {
+  const res = await axiosInst.get(`/repos/${owner}/${repo}/stargazers`, {
+    headers: {
+      Accept: withTimeStamp
+        ? 'application/vnd.github.star+json'
+        : 'application/vnd.github+json',
+    },
+    params: {
+      per_page: perPage,
+      page: page,
+    },
+  });
+  return res.data;
+}
---
+++ src/types/github.d.ts
@@ -0,0 +1,42 @@
+/**
+ * Basic user response when listing stargazers without timestamps.
+ */
+type User = {
+  avatar_url: string;
+  events_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  gravatar_id: string;
+  html_url: string;
+  id: number;
+  login: string;
+  node_id: string;
+  organizations_url: string;
+  received_events_url: string;
+  repos_url: string;
+  site_admin: boolean;
+  starred_url: string;
+  subscriptions_url: string;
+  type: string;
+  url: string;
+};
+
+/**
+ * Response type when listing stargazers with timestamps.
+ */
+type Starred = {
+  starred_at?: string;
+  user: User;
+};
+
+/**
+ * Input parameters for getStargazers function with or without timestamps.
+ */
+type getStargazersPayload = {
+  owner: string;
+  repo: string;
+  perPage: number;
+  page: number;
+  withTimeStamp?: boolean;
+};
