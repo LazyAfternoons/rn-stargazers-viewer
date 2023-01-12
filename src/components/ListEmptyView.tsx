ec51c8957b6403d459358f6ecd1a2ef0399adb1d
LazyAfternoons <lazyafternoons@outlook.it>
2023-01-12 22:53:23 +0100

    fix: empty list message not centered

--- src/components/ListEmptyView.tsx
+++ src/components/ListEmptyView.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import {Text, View} from 'react-native';
+import {useTextStyles} from '../themes/theme';
 
 /**
  * A view for empty stargazers list.
  */
 const ListEmptyView = () => {
   const {t} = useTranslation();
+  const textStyles = useTextStyles();
   return (
     <View>
-      <Text>{t('list.empty')}</Text>
+      <Text style={textStyles.centered}>{t('list.empty')}</Text>
     </View>
   );
 };
