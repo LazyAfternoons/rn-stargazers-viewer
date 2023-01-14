0bdc22c06725c4a3dc5845019c9c41b5c4f13618
LazyAfternoons <lazyafternoons@outlook.it>
2023-01-14 12:07:58 +0100

    feat: move the form submit button to the bottom of the screen

--- src/components/RepoInputForm.tsx
+++ src/components/RepoInputForm.tsx
@@ -2,7 +2,13 @@ import {Button, CheckBox, Input} from '@rneui/themed';
 import React from 'react';
 import {Controller, useForm} from 'react-hook-form';
 import {useTranslation} from 'react-i18next';
-import {StyleProp, View, ViewStyle} from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleProp,
+  View,
+  ViewStyle,
+} from 'react-native';
 
 /**
  * Type for data passed by the form handler.
@@ -31,9 +37,19 @@ export type RepoFormProps = {
    */
   handler: (data: RepoFormData) => void;
   /**
-   * Optional container style for the outer view.
+   * Optional container style for the outer view of the whole form.
    */
   containerStyle?: StyleProp<ViewStyle>;
+
+  /**
+   * Optional container style for the fiels outer view.
+   */
+  fieldsContainerStyle?: StyleProp<ViewStyle>;
+
+  /**
+   * Optional container style for the submit outer view.
+   */
+  submitContainerStyle?: StyleProp<ViewStyle>;
 };
 
 /**
@@ -43,9 +59,14 @@ export type RepoFormProps = {
  * owner: the value is require and the max lenght is 38.
  * repo: the vaule is required and the max length is 100.
  * Unfortunately there's no official documentation on limits and validation rules of username and repo names.
- * Only lenght is restricted due to being very easy to very by using the web interface.
+ * Only lenght is restricted due to being very easy to verify by using the web interface.
  */
-const RepoInputForm = ({handler, containerStyle}: RepoFormProps) => {
+const RepoInputForm = ({
+  handler,
+  containerStyle,
+  fieldsContainerStyle,
+  submitContainerStyle,
+}: RepoFormProps) => {
   const {
     control,
     handleSubmit,
@@ -57,63 +78,69 @@ const RepoInputForm = ({handler, containerStyle}: RepoFormProps) => {
 
   return (
     <View style={containerStyle}>
-      <Controller
-        control={control}
-        rules={{
-          required: {value: true, message: t('form.required')},
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            autoCapitalize="none"
-            label={'Owner'}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            autoCorrect={false}
-            maxLength={38}
-            errorMessage={errors?.owner?.message}
-          />
-        )}
-        name="owner"
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={fieldsContainerStyle}>
+        <Controller
+          control={control}
+          rules={{
+            required: {value: true, message: t('form.required')},
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              autoCapitalize="none"
+              label={'Owner'}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCorrect={false}
+              maxLength={38}
+              errorMessage={errors?.owner?.message}
+            />
+          )}
+          name="owner"
+        />
 
-      <Controller
-        control={control}
-        rules={{
-          maxLength: {value: 250, message: t('form.repo_maxlength')},
-          required: {value: true, message: t('form.required')},
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <Input
-            autoCapitalize="none"
-            label={'Name'}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-            autoCorrect={false}
-            maxLength={100}
-            errorMessage={errors?.repo?.message}
-          />
-        )}
-        name="repo"
-      />
+        <Controller
+          control={control}
+          rules={{
+            maxLength: {value: 250, message: t('form.repo_maxlength')},
+            required: {value: true, message: t('form.required')},
+          }}
+          render={({field: {onChange, onBlur, value}}) => (
+            <Input
+              autoCapitalize="none"
+              label={'Name'}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              autoCorrect={false}
+              maxLength={100}
+              errorMessage={errors?.repo?.message}
+            />
+          )}
+          name="repo"
+        />
 
-      <Controller
-        control={control}
-        render={({field: {value}}) => (
-          <CheckBox
-            title={t('form.include_ts') || ''} //always returns something, fallsback to form.include_ts
-            checked={value}
-            onPress={() => setValue('withTimestamp', !value)}
-          />
-        )}
-        name="withTimestamp"
-      />
-      <Button
-        disabled={!isValid}
-        title={submitLabel}
-        onPress={handleSubmit(handler)}
-      />
+        <Controller
+          control={control}
+          render={({field: {value}}) => (
+            <CheckBox
+              title={t('form.include_ts') || ''} //always returns something, fallsback to form.include_ts
+              checked={value}
+              onPress={() => setValue('withTimestamp', !value)}
+            />
+          )}
+          name="withTimestamp"
+        />
+      </KeyboardAvoidingView>
+      <View style={submitContainerStyle}>
+        <Button
+          disabled={!isValid}
+          title={submitLabel}
+          onPress={handleSubmit(handler)}
+        />
+      </View>
     </View>
   );
 };
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {KeyboardAvoidingView, Platform, StyleSheet} from 'react-native';
+import {Keyboard, Pressable, StyleSheet, View} from 'react-native';
 import RepoInputForm, {RepoFormData} from '../components/RepoInputForm';
 import Header from '../components/Header';
 import {SafeAreaView} from 'react-native-safe-area-context';
@@ -24,19 +24,25 @@ const Home = ({dispatch, navigation}: HomeProps) => {
   const submitHandler = (data: RepoFormData) => {
     dispatch(resetStargazers());
     navigation.navigate('List', {...data});
+    Keyboard.dismiss();
   };
 
   return (
-    <SafeAreaView
-      edges={['top', 'right', 'left', 'bottom']}
-      style={viewStyle.safeArea}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={viewStyle.paddedHeader}>
-        <Header />
-        <RepoInputForm handler={submitHandler} />
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+    <Pressable style={viewStyle.pressable} onPress={() => Keyboard.dismiss()}>
+      <SafeAreaView
+        edges={['top', 'right', 'left', 'bottom']}
+        style={viewStyle.safeArea}>
+        <View style={viewStyle.paddedOuter}>
+          <Header />
+          <RepoInputForm
+            containerStyle={formStyle.container}
+            fieldsContainerStyle={formStyle.fields}
+            submitContainerStyle={formStyle.submit}
+            handler={submitHandler}
+          />
+        </View>
+      </SafeAreaView>
+    </Pressable>
   );
 };
 
@@ -51,9 +57,25 @@ export default connect(mapStateToProps)(Home);
 const viewStyle = StyleSheet.create({
   safeArea: {
     flex: 1,
-    justifyContent: 'space-between',
   },
-  paddedHeader: {
+  paddedOuter: {
     padding: 25,
+    flex: 1,
+  },
+  pressable: {
+    flex: 1,
+  },
+});
+
+const formStyle = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  fields: {
+    justifyContent: 'flex-start',
+  },
+  submit: {
+    justifyContent: 'flex-end',
+    flex: 1,
   },
 });
