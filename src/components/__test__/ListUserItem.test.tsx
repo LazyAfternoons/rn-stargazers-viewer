83d3bf7309dfab3e1a59c3f9c3a0c57ac8f5b46b
LazyAfternoons <lazyafternoons@outlook.it>
2023-01-15 15:51:41 +0100

    test: adding missing ListUserItem avatar test

--- src/components/__test__/ListUserItem.test.tsx
+++ src/components/__test__/ListUserItem.test.tsx
@@ -34,7 +34,20 @@ describe('ListUserItem', () => {
       expect(component.queryByText(mockedStarred.user.login)).not.toBeNull();
     });
 
-    it('should render timestamp', () => {
+    it('should render avatar', () => {
+      const component = render(<ListUserItem info={mockedUser} />);
+      const avatarView = component.queryByTestId('avatarView');
+      const avatar = avatarView?.children[0];
+      if (avatar !== undefined && typeof avatar !== 'string') {
+        expect(
+          avatar.props.source.uri === mockedStarred.user.avatar_url,
+        ).toBeTruthy();
+      } else {
+        fail('Avatar not rendered');
+      }
+    });
+
+    it('should not render timestamp', () => {
       const component = render(<ListUserItem info={mockedUser} />);
       expect(component.queryByTestId('timestamp')).not.toBeUndefined();
     });
